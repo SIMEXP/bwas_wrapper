e4209ad6d21cbd4de2a7afc5b72d6efd747e41a6
pbellec <pierre.bellec@gmail.com>
2020-05-16 08:06:14 -0400

    Basic package structure now working.

--- bwas_wrapper/__init__.py
+++ bwas_wrapper/__init__.py
@@ -1,6 +1,4 @@
 """BWAS wrapper."""
 from bwas_wrapper.wrapper import wrapper
-from dypac.embeddings import Embedding
-from dypac.bascpp import replicate_clusters, find_states, stab_maps
-from dypac.tests import test_bascpp, test_dypac
-__all__ = ['Dypac', 'test_bascpp', 'test_dypac', 'replicate_clusters', 'find_states', 'stab_maps', 'Embedding']
+from bwas_wrapper.BWAS import BWAS_cpu
+__all__ = ['wrapper', 'BWAS_cup']
---
+++ bwas_wrapper/wrapper.py
@@ -0,0 +1,16 @@
+from bwas_wrapper import BWAS_cup
+
+def wrapper(df):
+    """
+    A wrapper for the BWAS library.
+
+    Parameters
+    ----------
+    :paramater df: a pandas data frame. Each row is a subject. The first column will be treated as the variable "of interest" in a regression model. This would be for example a dummy variable with 0s and 1s for a group comparison. The other columns will be used as confonds in the analysis. The wrapper expects to find a column called "files", with the name of a nifti file for that subject. This column will not be excluded from the analysis.
+
+    Note
+    ----
+    The files need to point to *fully preprocessed* 4D nifti files. No detrending or smoothing can be applied.
+    """
+    a = 1
+    return a
