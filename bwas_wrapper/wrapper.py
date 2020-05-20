20a0e57d881c16598036252beb09bf3a4a03b004
pbellec <pierre.bellec@gmail.com>
2020-05-20 08:14:07 -0400

    Started implementing the wrapper.

--- bwas_wrapper/wrapper.py
+++ bwas_wrapper/wrapper.py
@@ -1,16 +1,37 @@
+import os
+import tempfile
 from bwas_wrapper.BWAS import BWAS_cpu
 
-def wrapper(df):
+def wrapper(model, files, output_folder, subject_id, interest):
     """
     A wrapper for the BWAS library.
 
     Parameters
     ----------
-    :paramater df: a pandas data frame. Each row is a subject. The first column will be treated as the variable "of interest" in a regression model. This would be for example a dummy variable with 0s and 1s for a group comparison. The other columns will be used as confonds in the analysis. The wrapper expects to find a column called "files", with the name of a nifti file for that subject. This column will not be excluded from the analysis.
+    :paramater model: each column is a regressor, and each row is a subject.
+    :type model: pandas data frame
+    :parameter files: files[subject] is the file associated with subject
+    :type files: dictionary
+    :parameter output_folder: where to save the results of the analysis.
+    :type output_folder: string.
+    :parameter subject_id: the name of the column in model containing subject IDs
+    :type subject_id: string
+    :parameter interest: the name of the column of interest in model.
+    This would be for example a dummy variable with 0s and 1s for a group
+    comparison. The other columns will be used as confonds in the analysis.
+    :type interest: string
 
     Note
     ----
     The files need to point to *fully preprocessed* 4D nifti files. No detrending or smoothing can be applied.
     """
-    a = 1
-    return a
+    path_analysis = tempfile.TemporaryDirectory()
+    print("Creating sym links in the following path: {0}".format(path_analysis.name))
+    for idx, subject in enumerate(model[subject_id]):
+        file = f'{idx: 12}.nii.gz'.replace(' ','0')
+        os.symlink(src=files[subject], dst=os.path.join(path_analysis.name, file))
+
+
+    BWAS_run_full_analysis(result_dir,image_dir,mask_file,toolbox_path,targets_file,cov_file,CDT,memory_limit_per_core=16,ncore=1)
+
+    path_analysis.cleanup()
